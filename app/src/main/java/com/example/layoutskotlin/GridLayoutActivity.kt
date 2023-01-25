cdb27a54deae3801ad0a674def4515bdde43da17
Anurag Modi <anuragmodi@Anurags-MacBook-Air.local>
2023-01-25 10:46:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/layoutskotlin/GridLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.example.layoutskotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GridLayoutActivity  : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.grid_layout)
+    }
+}
---
+++ app/src/main/java/com/example/layoutskotlin/LinearLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.layoutskotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LinearLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_layout)
+    }
+}
